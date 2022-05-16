548000491878d734bea4ee75ee7e3fe25f79b615
Ryosuke-SagaU <19238346@edu.cc.saga-u.ac.jp>
2022-05-16 15:37:37 +0900

    [Grid] create grid line picture

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import numpy as np
 # 画像の読み込み パスをどこに設定する?
 # エスケープシーケンス(/n 改行などを表す)と判断されるので、これを無効化させる。
 # / → // で表現する
-img = cv2.imread("C:\\Users\\Ryosuke Nikushi\\picture.png")
+img = cv2.imread("C:\\Users\\Ryosuke Nikushi\\GrayPicture.png")
 
 #y_step=50
+#x_step=50
 y_step=40
-x_step=50
+x_step=20
 
 img_y,img_x=img.shape[:2]
 
