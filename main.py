f3b6a7afa2090df7a1c1cd3eb73c289d4d062aa2
Ryosuke-SagaU <19238346@edu.cc.saga-u.ac.jp>
2022-05-16 15:53:26 +0900

    [Remove] remove picture

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ import numpy as np
 # 画像の読み込み パスをどこに設定する?
 # エスケープシーケンス(/n 改行などを表す)と判断されるので、これを無効化させる。
 # / → // で表現する
-img = cv2.imread("C:\\Users\\Ryosuke Nikushi\\GrayPicture.png")
+img = cv2.imread("C:\\Users\\Ryosuke Nikushi\\GrayPicture-40x20.png")
 
 #y_step=50
 #x_step=50
-y_step=40
-x_step=20
+#y_step=40
+#x_step=20
 
-img_y,img_x=img.shape[:2]
+#img_y,img_x=img.shape[:2]
 
-img[y_step:img_y:y_step, :, :] = 255
-img[:, x_step:img_x:x_step, :] = 255
+#img[y_step:img_y:y_step, :, :] = 255
+#img[:, x_step:img_x:x_step, :] = 255
 
-cv2.imwrite("C:\\Users\\Ryosuke Nikushi\\newpicture.png", img)
+# [左がy座標,右がx座標]
+img1 = img[80 : 360, 20 : 200]
+cv2.imwrite("C:\\Users\\Ryosuke Nikushi\\newpicture.png", img1)
 
 #img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
