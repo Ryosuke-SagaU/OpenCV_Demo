11afcf9b878edd5bbc3f92e2005e78e1e9f86422
Ryosuke-SagaU <19238346@edu.cc.saga-u.ac.jp>
2022-05-16 16:05:02 +0900

    [create] create more detailed grid picture

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ import numpy as np
 # 画像の読み込み パスをどこに設定する?
 # エスケープシーケンス(/n 改行などを表す)と判断されるので、これを無効化させる。
 # / → // で表現する
-img = cv2.imread("C:\\Users\\Ryosuke Nikushi\\GrayPicture-40x20.png")
+img = cv2.imread("C:\\Users\\Ryosuke Nikushi\\GrayPicture-80,360-20,200.png", 0)
+
+ret2, img_otsu = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
+
+print("ret2: {}".format(ret2))
+
+cv2.imshow("otsu", img_otsu)
+cv2.imwrite("C:\\Users\\Ryosuke Nikushi\\newpicture.png", img_otsu)
+cv2.waitKey()
+cv2.destroyAllWindows()
 
 #y_step=50
 #x_step=50
@@ -17,8 +26,8 @@ img = cv2.imread("C:\\Users\\Ryosuke Nikushi\\GrayPicture-40x20.png")
 #img[:, x_step:img_x:x_step, :] = 255
 
 # [左がy座標,右がx座標]
-img1 = img[80 : 360, 20 : 200]
-cv2.imwrite("C:\\Users\\Ryosuke Nikushi\\newpicture.png", img1)
+#img1 = img[80 : 360, 20 : 200]
+#cv2.imwrite("C:\\Users\\Ryosuke Nikushi\\newpicture.png", img1)
 
 #img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
